b6799d0c9df525b477366fae397fcb02e666abf9
Gyarmati Tamás <tamasgy56@gmail.com>
2026-01-14 17:59:32 +0100

    Fixed the backend logic, again

--- BACKEND/BackendASP/BackendASP/Services/RoomPlannerService.cs
+++ BACKEND/BackendASP/BackendASP/Services/RoomPlannerService.cs
@@ -11,6 +11,17 @@ public class RoomPlannerService(IRoomRepository roomRepo, IItemRepository itemRe
 
         var planner = new RoomPlanner(roomData.Height, roomData.Width);
 
-        return items.Select(item => planner.TryPlaceFurniture(item)).OfType<Coordinates>().ToList();
-    }
+        var coordinates = new List<Coordinates>();
+
+        foreach (var item in items)
+        {
+            var position = planner.TryPlaceFurniture(item);
+            if (position != null)
+            {
+                coordinates.Add(position);
+            }
+        }
+
+        return coordinates;
+    }    
 }
